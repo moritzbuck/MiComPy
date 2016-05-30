2a898bb97d1bc89a4743a24c0a99e1fba9405458
moritzbuck <mrtz.buck@gmail.com>
2016-05-30 13:59:00 +0200

    added parser for hmmer

--- micompy/gene_clusterings/pfam_clusters/clustering.py
+++ micompy/gene_clusterings/pfam_clusters/clustering.py
@@ -13,3 +13,22 @@ class PfamClustering(Clustering):
 		call(" ".join(["cat"] + self.proteoms  + [ " > " + pjoin(temp_loc, "full_proteom.faa")]) 
 		shutil.copy(pfama_loc, temp_loc)   	
     	call(["hmmsearch --cpu 16 -o hmmer_good_prots.raw Pfam-A.hmm" , pjoin(temp_loc, "full_proteom.faa") ])
+
+    def parse_hmmer_results(self, file):
+
+
+		entries = []
+		t = []
+		post = False
+		with open(infile) as handle:
+		    print "Parsing hmmsearch output!"
+		    for l in tqdm(handle.readlines()):
+		        if l[0:17] == "Domain annotation" and not post :
+		            post = True
+		        if l[0] != "#" and len(l[:-1]) > 0 and not post :
+		            t.append(l[:-1])
+		        if l == "//\n":
+		            entries.append(t)
+		            t = []
+		            post = False 
+		
